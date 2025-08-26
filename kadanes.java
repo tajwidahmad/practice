8fefc4b49b5913b9e798921a5170168ff1576c68
tajwidahmad <tajwidahmad5@gmail.com>
2025-08-26 12:18:05 +0530

    kadanes algorithm

---
+++ kadanes.java
@@ -0,0 +1,19 @@
+public class kadanes {
+    public static int sub_sum(int arr[]) {
+        int max_sum=Integer.MIN_VALUE;
+        int curr_sum=0;
+        for(int i=0;i<arr.length;i++){
+            curr_sum+=arr[i];
+            if (curr_sum<0) {
+                curr_sum=0;
+                
+            }
+            max_sum=Math.max(curr_sum, max_sum);
+        }
+        return max_sum;
+    }
+    public static void main(String[] args) {
+        int arr[]={-2,-3,4,-1,-2,1,5,-3};
+        System.out.println(sub_sum(arr));
+    }
+}
