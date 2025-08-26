ff76d134196b6619b818dbd5109b0f1c8195c49c
tajwidahmad <tajwidahmad5@gmail.com>
2025-08-26 11:50:23 +0530

    yes

--- nrp.java
+++ nrp.java
@@ -1,25 +1,34 @@
 public class nrp {
     public static int non_repeat(int arr[]) {
   
-        for(int i=0;i<arr.length;i++){
-            int count=0;
-            for(int j=0;j<arr.length;j++){
-                if (arr[i]==arr[j]) {
-                    count++;
+        // for(int i=0;i<arr.length;i++){
+        //     int count=0;
+        //     for(int j=0;j<arr.length;j++){
+        //         if (arr[i]==arr[j]) {
+        //             count++;
                     
-                }
+        //         }
 
-            }
-            if (count==1) {
-                return arr[i];
+        //     }
+        //     if (count==1) {
+        //         return arr[i];
                 
-            }
+        //     }
+        // }
+        // return -1;
+        int non_repeated=arr[0];
+        for(int i=1;i<arr.length;i++){
+            non_repeated=non_repeated ^ arr[i];
         }
-        return -1;
+        return non_repeated;
+
+
+
         
     }
     public static void main(String[] args) {
-        int arr[]={1,2,4,1,2,4,1};
+        int arr[]={1,2,2,7,1,4,4};
+        // System.out.println(non_repeat(arr));
         System.out.println(non_repeat(arr));
     }
 }
