6dcc9d4411f1c9c02a76e04ea26869b92f43f4e9
tajwidahmad <tajwidahmad5@gmail.com>
2025-10-12 20:26:31 +0530

    strings

---
+++ strings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class strings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter youe name :");
+        String name = sc.next();
+        System.out.println(name);
+        
+        
+    }
+}
