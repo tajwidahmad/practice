1f33c570a57069c146d33525a267c8ec24498f9e
tajwidahmad <tajwidahmad5@gmail.com>
2025-10-12 20:28:19 +0530

    string2

--- strings.java
+++ strings.java
@@ -1,11 +1,13 @@
 import java.util.*;
 public class strings {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter youe name :");
-        String name = sc.next();
-        System.out.println(name);
-        
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("enter youe name :");
+        // String name = sc.next();
+        // System.out.println(name);
+        String str= "ahmad";
+        System.out.println(str.charAt(3));
+
         
     }
 }
