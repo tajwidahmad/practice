034de81f08dd36c89cb53613274770f539aff340
tajwidahmad <tajwidahmad5@gmail.com>
2025-10-25 09:24:54 +0530

    revision functions part 2

--- prime.java
+++ prime.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class prime {
-    // public static void main(String[] args) {
-    //     Scanner sc= new Scanner(System.in);
-    //     int num=sc.nextInt();
-    //     // System.out.println(check(num));
-    //     // printing the prime numbers in between 
-    //     for(int i=2;i<num;i++){
-    //         if (check(i)==true) {
-    //             System.out.println(i);
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num=sc.nextInt();
+        // System.out.println(check(num));
+        // printing the prime numbers in between 
+        for(int i=2;i<num;i++){
+            if (check(i)==true) {
+                System.out.println(i);
                 
-    //         }
-    //     }
+            }
+        }
         
         
         
