4388071ff13eaaf7e546abd3bb8e27502f745eb6
tajwidahmad <tajwidahmad5@gmail.com>
2025-10-28 19:47:29 +0530

    maximum repeated element in the arrays

--- leetcode1.java
+++ leetcode1.java
@@ -4,6 +4,7 @@ public class leetcode1 {
         int arr[]={1,1,1,2,2,3};
         System.out.println(maxrepeatedele(arr));
     }
+    // function for the max repeated element in the array
     public static int maxrepeatedele(int arr[]) {
         int count=1;
         for(int i=0;i<arr.length;i++){
