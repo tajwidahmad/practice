0b876a6daa28e2ea220519412af211586fac2be3
tajwidahmad <tajwidahmad5@gmail.com>
2025-10-28 21:00:21 +0530

    nqt 3

--- nqt3.java
+++ nqt3.java
@@ -1,5 +1,22 @@
 public class nqt3 {
     public static void main(String[] args) {
-        System.out.println("hello");
+        //swapping for reversing an array
+        int arr[]={10,20,30,40,50};
+        reverse(arr);
+
+    }
+    public static void reverse(int arr[]) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
     }
 }
